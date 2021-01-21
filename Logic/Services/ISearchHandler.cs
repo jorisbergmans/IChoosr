e75852d968aa7f8f72d1394778da4db87393df76
Joris Bergmans <joris.bergmans@hotmail.com>
2021-01-21 19:47:11 +0100

    Added missing files

---
+++ Logic/Services/ISearchHandler.cs
@@ -0,0 +1,13 @@
+﻿using Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Services
+{
+    public interface ISearchHandler
+    {
+        void ShowCamerasByName(string name);
+        List<CameraModel> GetNames(string name);
+    }
+}
