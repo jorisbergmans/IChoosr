c894ae0ab095e5950b9f0e3f7d9d545419693397
Joris Bergmans <joris.bergmans@hotmail.com>
2021-01-21 19:42:22 +0100

    Fix for Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Logic.Implementations;
+using Logic.Services;
+using Logic.Services.Implementations;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CLISearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup for the DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<ISearchHandler, SearchHandler>()
+                .AddSingleton<ICSVHelper, CSVHelper>()
+                .BuildServiceProvider();
+
+            //Setup for the logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .CreateLogger("Debug");
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            //Executing the logic
+            var search = serviceProvider.GetService<ISearchHandler>();
+            Console.WriteLine(args[0]);
+            if (args.Length != 0)
+            {
+                search.ShowCamerasByName(args[1]);
+            }
+
+            logger.LogDebug("Finished succesfully!");
+        }
+    }
+}
